dc3e8e14c6250cd1c1e4d7f4f669508306fa09f0
Lembani Sakala <lembanisakala@gmail.com>
2022-04-06 11:52:22 +0200

    Abstract toggle menu & adjust links to the different sections of the page

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const logo = document.querySelector(".logo");
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close");
 const options = document.querySelector(".options");
-const portfolio = document.querySelector(".portfolio");
-const about = document.querySelector(".about");
-const contact = document.querySelector(".contact");
+const portfolio = document.querySelector("#portfolio-link");
+const about = document.querySelector("#about-link");
+const contact = document.querySelector("#contact-link");
 
 closeIcon.style.display = "none";
 options.style.display = "none";
 
-mobileIcons.addEventListener("click", () => {
+const toggleMenuOptions = () => {
 	if (nav.classList.contains("click-mobile-icon")) {
 		nav.classList.remove("click-mobile-icon");
 		closeIcon.style.display = "none";
@@ -25,4 +25,9 @@ mobileIcons.addEventListener("click", () => {
 		logo.style.display = "none";
 		options.style.display = "block";
 	}
-});
+}
+
+mobileIcons.addEventListener("click", toggleMenuOptions);
+portfolio.addEventListener('click', toggleMenuOptions);
+about.addEventListener('click', toggleMenuOptions);
+contact.addEventListener('click', toggleMenuOptions);
