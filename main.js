7e04b1ca60560bfa13f3fdeca990f75cdadc7165
dd-obua <dd.obua@gmail.com>
2022-04-13 18:44:23 +0300

    Add project in project section

--- main.js
+++ main.js
@@ -31,3 +31,28 @@ mobileIcons.addEventListener('click', toggleMenuOptions);
 portfolio.addEventListener('click', toggleMenuOptions);
 about.addEventListener('click', toggleMenuOptions);
 contact.addEventListener('click', toggleMenuOptions);
+
+// generate multi stories section using JavaScript
+const stories = [
+  {
+    empty: '',
+    storyTitle: 'Multi-Post Stories Gain+Glory',
+    languages: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    storyDetails: {
+      name: 'Keeping track of hundreds of components',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      featuredImage: './images/desktop-featrured-image.svg',
+      technologies: [
+        'CodeKit',
+        'GitHub',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'CodePen',
+      ],
+      liveVersionLink: 'https://dd-obua.github.io/Dan-portfolio/',
+      sourceLink: 'https://github.com/dd-obua/Dan-portfolio',
+    },
+  },
+];
